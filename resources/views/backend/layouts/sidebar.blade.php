a80d50133540ff09118fece157615eac4795cab0
codepundit1 <jahid35-266@diu.edu.bd>
2022-03-21 21:34:16 +0600

    menu active done

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -19,7 +19,10 @@
             </div>
           </div>
 
-
+          @php
+              $prefix = Request::route()->getPrefix();
+              $route = Route::current()->getName();
+          @endphp
 
           <!-- Sidebar Menu -->
           <nav class="mt-2">
@@ -27,7 +30,7 @@
               <!-- Add icons to the links using the .nav-icon class
                  with font-awesome or any other icon font library -->
               @if (Auth::user()->userType == 'Admin')
-                 <li class="nav-item">
+                 <li class="nav-item has-treeview {{ ($prefix=='/users')?'menu-open':'' }}">
                 <a href="#" class="nav-link">
                     <i class="fa fa-user fa-fw mr-1"></i>
                   <p>
@@ -39,27 +42,22 @@
                 </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <a href="{{route('users.view')}}" class="nav-link">
+                    <a href="{{route('users.view')}}" class="nav-link {{ ($route == 'users.view')?'active':'' }}">
                       <i class="far fa-circle nav-icon"></i>
                       <p>View Users</p>
                     </a>
                   </li>
 
-                  <li class="nav-item">
-                    <a href="pages/layout/boxed.html" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Boxed</p>
-                    </a>
-                  </li>
+
 
 
                 </ul>
               </li>
               @endif
-             
 
-              <li class="nav-item">
-                <a href="#" class="nav-link">
+
+              <li class="nav-item has-treeview {{ ($prefix=='/profiles')?'menu-open':'' }}">
+                <a href="#" class="nav-link ">
                     <i class="fa fa-user fa-fw mr-1"></i>
                   <p>
 
@@ -70,7 +68,7 @@
                 </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <a href="{{route('profiles.view')}}" class="nav-link">
+                    <a href="{{route('profiles.view')}}" class="nav-link {{ ($route == 'profiles.view')?'active':'' }}">
                       <i class="far fa-circle nav-icon"></i>
                       <p>My Profile</p>
                     </a>
@@ -79,7 +77,7 @@
 
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <a href="{{route('profiles.password.view')}}" class="nav-link">
+                    <a href="{{route('profiles.password.view')}}" class="nav-link {{ ($route == 'profiles.password.view')?'active':'' }}">
                       <i class="far fa-circle nav-icon"></i>
                       <p>Change Password</p>
                     </a>
@@ -87,25 +85,7 @@
                 </ul>
               </li>
 
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon fas fa-copy"></i>
-                  <p>
-                    Frontend Sitting
-                    <i class="fas fa-angle-left right"></i>
 
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Bannel Slider</p>
-                    </a>
-                  </li>
-
-                </ul>
-              </li>
 
             </ul>
           </nav>
